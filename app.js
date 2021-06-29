edf5a72567723d6baf697117922c7195b33b0a56
andresciccarelli <aciccarelli@gmail.com>
2021-06-28 22:12:33 -0300

    home bichi

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('home', {
         nombre: 'Andres',
-        titulo: 'Curso de Node'
+        titulo: 'Home bichi'
 
     });
 });
